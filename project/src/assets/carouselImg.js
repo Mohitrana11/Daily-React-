15752512aa4bf4fa131a3a04e3d2e7cf1d5f2ccc
Mohit SIngh Rana <mohitranamohhitrana1127@gmail.com>
2024-08-23 15:49:12 -0500

    Today i created a progress bar

---
+++ project/src/assets/carouselImg.js
@@ -0,0 +1,43 @@
+const carouselImgs = [
+    {
+        image:'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(31).webp',
+        
+    },
+    {
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu0JAMPtLKtfmRY0TApaXq3s80-4RacHL1lA&s',
+        
+    },
+    {
+        image:'https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg',
+        
+    },
+    {
+        image:'https://khuuba.com.au/cdn/shop/articles/Saree_Blouse_Designs.jpg?crop=center&height=666&v=1686304584&width=1600',
+       
+    }
+    ,
+    {
+        image:'https://www.shutterstock.com/image-vector/3d-yellow-great-discount-sale-260nw-2056851839.jpg',
+    },
+    {
+        image:'https://khuuba.com.au/cdn/shop/articles/Hairstyles_for_saree.jpg?crop=center&height=800&v=1698319808&width=1920',
+        
+    },
+    {
+        image:'https://khuuba.com.au/cdn/shop/articles/Saree_Blouse_Designs.jpg?crop=center&height=666&v=1686304584&width=1600',
+       
+    }
+    ,
+    {
+        image:'https://www.shutterstock.com/image-vector/3d-yellow-great-discount-sale-260nw-2056851839.jpg',
+    },
+    {
+        image:'https://khuuba.com.au/cdn/shop/articles/Saree_Blouse_Designs.jpg?crop=center&height=666&v=1686304584&width=1600',
+       
+    }
+    ,
+    {
+        image:'https://www.shutterstock.com/image-vector/3d-yellow-great-discount-sale-260nw-2056851839.jpg',
+    }
+]
+export default carouselImgs;
